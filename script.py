00f03fac24677799e6338e0d8a1036fa53db4695
ayaanrizv <66327527+ayaanrizv@users.noreply.github.com>
2021-06-17 15:06:53 +0530

    Update script.py

--- script.py
+++ script.py
@@ -1,7 +1,7 @@
 from translate import Translator
 translator = Translator(to_lang="en",from_lang="autodetect")
 try:
-	with open('lyrics3.txt', mode='r') as my_file:
+	with open('lyrics.txt', mode='r') as my_file:
 		text = my_file.read()
 		translation = translator.translate(text)
 	with open('translated-lyrics.txt', mode='w') as my_file2:
